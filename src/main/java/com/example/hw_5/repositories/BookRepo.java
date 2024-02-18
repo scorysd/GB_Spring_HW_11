4794a117406488061e4d10ff0638dc2c048e0342
Roman <toolatet@gmail.com>
2024-02-18 18:03:31 +0700

    init

--- src/main/java/com/example/hw_5/repositories/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.hw_5.repositories;
-
-import com.example.hw_5.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book,Long> {
-}
--- src/main/java/com/example/hw_5/repositories/IssuesBookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.hw_5.repositories;
-
-import com.example.hw_5.models.IssuesBooks;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssuesBookRepo extends JpaRepository<IssuesBooks, Long> {
-}
